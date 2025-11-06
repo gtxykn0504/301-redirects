6dde9e42026aab5e85f8b2af541401dc917fe20c
He <i@zhhe.dev>
2025-11-06 10:32:37 +0800

    chore: link update

--- lib/redirect-config.ts
+++ lib/redirect-config.ts
@@ -3,11 +3,15 @@ export const redirectRules = {
     type: "full",
     target: "https://myapps.microsoft.com/?whr=spircape.com",
   },
+  "box.ofhe.cn": {
+    type: "full",
+    target: "https://box.zhhi.cn",
+  },
   "go.ofhe.cn": {
     type: "path",
     routes: {
-      "/410photo": "https://box.ofhe.cn/s/5Qt9",
-      "/159-20250930": "https://box.ofhe.cn/s/JGSV",
+      "/410photo": "https://box.zhhi.cn/s/dGHb",
+      "/159-20250930": "https://box.zhhi.cn/s/yvib",
     },
   },
 }
